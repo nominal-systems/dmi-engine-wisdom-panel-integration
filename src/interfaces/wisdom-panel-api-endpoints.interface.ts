41b92922995c38e9df22f8b5998d972d71d0bb86
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-04-11 10:59:00 -0300

    Results processor

--- src/interfaces/wisdom-panel-api-endpoints.interface.ts
+++ src/interfaces/wisdom-panel-api-endpoints.interface.ts
@@ -1,5 +1,7 @@
 export enum WisdomPanelApiEndpoints {
   AUTH = '/oauth/token',
   GET_KITS = '/api/v1/kits',
+  GET_RESULT_SETS = '/api/v1/result-sets',
+  GET_SIMPLIFIED_RESULT_SETS = '/api/voyager/banfield-results-retrieval',
   CREATE_PET = '/api/voyager/pet',
 }
