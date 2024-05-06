db8cf01acd287124978a325cf3381a065c0b3a01
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-05-06 16:38:07 +0200

    0.2.7

--- src/providers/wisdom-panel-mapper.spec.ts
+++ src/providers/wisdom-panel-mapper.spec.ts
@@ -246,7 +246,7 @@ describe('WisdomPanelMapper', () => {
       items.forEach((item, index) => {
         const breedPercentage: WisdomPanelBreedPercentagesResult = simpleResult.data.breed_percentages?.[
           index
-          ] as WisdomPanelBreedPercentagesResult
+        ] as WisdomPanelBreedPercentagesResult
         expect(item.seq).toEqual(index)
         expect(item.code).toEqual(breedPercentage.breed.slug)
         expect(item.name).toEqual(breedPercentage.breed.name.en)
