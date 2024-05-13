10e734bd29fe1443f97b4b400e3297847ffd70a0
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-05-13 14:41:05 +0200

    Wisdom API Exception

--- src/filters/rcp-exception-filter.ts
+++ src/filters/rcp-exception-filter.ts
@@ -1,10 +1,24 @@
-import { Catch, ExceptionFilter } from '@nestjs/common'
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common'
 import { RpcException } from '@nestjs/microservices'
+import { Observable, of, throwError } from 'rxjs'
+import { ProviderError } from '@nominal-systems/dmi-engine-common'
+import { PROVIDER_NAME } from '../constants/provider-name'
+import { WisdomApiException } from '../exceptions/wisdom-api.exception'
 
 @Catch(RpcException)
 export class RpcExceptionFilter implements ExceptionFilter<RpcException> {
-  catch (exception: RpcException) {
-    // TODO(gb): handle the exception
-    console.log(`RpcExceptionFilter exception= ${JSON.stringify(exception, null, 2)}`) // TODO(gb): remove trace
+  private readonly logger = new Logger(RpcExceptionFilter.name)
+
+  catch (exception: WisdomApiException, host: ArgumentsHost): Observable<any> {
+    this.logger.error(exception)
+
+    const providerError: ProviderError = new ProviderError({
+      provider: PROVIDER_NAME,
+      code: exception.statusCode || 500,
+      message: exception.message,
+      error: exception.errors
+    })
+
+    return throwError(() => providerError)
   }
 }
