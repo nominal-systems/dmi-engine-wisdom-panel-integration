52c0b386717e85355d299ab81390bf49577c0f1d
monsieurbelbo <gonzalo.bellver@gmail.com>
2024-08-28 14:11:56 +0200

    0.3.2

--- src/common/mapper-utils.spec.ts
+++ src/common/mapper-utils.spec.ts
@@ -3,10 +3,10 @@ import { extractKitCode } from './mapper-utils'
 describe('mapper-utils', () => {
   describe('extractKitCode()', () => {
     it('should capitalize and trim the kit code', () => {
-        expect(extractKitCode({ KitCode: ' 1234 ' })).toBe('1234')
-        expect(extractKitCode({ KitCode: 'v-ss123 ' })).toBe('V-SS123')
-        expect(extractKitCode({ KitCode: ' v-abc3' })).toBe('V-ABC3')
-        expect(extractKitCode({ KitCode: 'v abaDDC ' })).toBe('V ABADDC')
+      expect(extractKitCode({ KitCode: ' 1234 ' })).toBe('1234')
+      expect(extractKitCode({ KitCode: 'v-ss123 ' })).toBe('V-SS123')
+      expect(extractKitCode({ KitCode: ' v-abc3' })).toBe('V-ABC3')
+      expect(extractKitCode({ KitCode: 'v abaDDC ' })).toBe('V ABADDC')
     })
   })
 })
