5f9e4d56d52511613d1f014fa56217fa9173b5c3
monsieurbelbo <gonzalo.bellver@gmail.com>
2025-03-03 16:47:03 +0100

    0.3.11

--- src/common/mapper-utils.spec.ts
+++ src/common/mapper-utils.spec.ts
@@ -11,35 +11,39 @@ describe('mapper-utils', () => {
   })
   describe('mapNotableAndAtRiskHealthTestResults()', () => {
     it('should use result_value when available', () => {
-      const results = [{
-        copies: 2,
-        result_value: 'TEST_VALUE',
-        resolved_result: 'RESOLVED_RESULT',
-        ui_description: 'test description',
-        health_test: {
-          slug: 'test-slug',
-          disease_name: {
-            en: 'Test Disease'
+      const results = [
+        {
+          copies: 2,
+          result_value: 'TEST_VALUE',
+          resolved_result: 'RESOLVED_RESULT',
+          ui_description: 'test description',
+          health_test: {
+            slug: 'test-slug',
+            disease_name: {
+              en: 'Test Disease'
+            }
           }
         }
-      }]
+      ]
 
       const items = mapNotableAndAtRiskHealthTestResults(results, 0)
       expect(items[0].valueString).toBe('TEST_VALUE')
     })
 
     it('should fallback to resolved_result when result_value is not available', () => {
-      const results = [{
-        copies: 2,
-        resolved_result: 'RESOLVED_RESULT',
-        ui_description: 'test description',
-        health_test: {
-          slug: 'test-slug',
-          disease_name: {
-            en: 'Test Disease'
+      const results = [
+        {
+          copies: 2,
+          resolved_result: 'RESOLVED_RESULT',
+          ui_description: 'test description',
+          health_test: {
+            slug: 'test-slug',
+            disease_name: {
+              en: 'Test Disease'
+            }
           }
         }
-      }]
+      ]
 
       const items = mapNotableAndAtRiskHealthTestResults(results, 0)
       expect(items[0].valueString).toBe('RESOLVED_RESULT')
